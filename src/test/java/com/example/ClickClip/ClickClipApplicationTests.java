6f8504d575d8775f18bd247f9159975894081451
Mei Chen <ohmayoh@gmail.com>
2023-02-01 12:01:36 +0100

    Created project ClickClip. Current dependencies: web, jpa, mysql driver, h2 database, lombok, hibernate validator

---
+++ src/test/java/com/example/ClickClip/ClickClipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClickClip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickClipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
